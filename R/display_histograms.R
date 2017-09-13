8c5b5065a4edb73cbcb45846c74c1308d660237b
jonydog <joao_cat@hotmail.com>
2017-09-13 21:48:24 +0100

    Now display histogram function DONE. 1- missing documentation

--- R/display_histograms.R
+++ R/display_histograms.R
@@ -1,14 +1,17 @@
 display_histogram <- function(histograms, type, obs.index){
   
   if( type=='bht' ){
-    hist(x = as.numeric(histograms[obs.index,]) , breaks = 100 )
-    return;
+    graphics::hist(x = as.numeric(histograms[[obs.index]]) , breaks = 100 )
   }
   
-  if( type=='dbht' ){
+  else if( type=='dbht' ){
     
-    return;
+    poison_histogram   <- histograms[[obs.index]]$poison
+    antidote_histogram <- histograms[[obs.index]]$antidote
+    
+    hist(poison_histogram, col=rgb(1,0,0,0.5), breaks = 100 , main='Antidote(blue) and Poison(red)', xlab='c-index variation');
+    hist(antidote_histogram, col=rgb(0,0,1,0.5),  breaks=100, add=T);
+    box()
   }
   
-  return;
 }
